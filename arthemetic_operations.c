6c64a51ff1a592844a1e0260dd34d74e422efdc8
SRIRAM N <93561991+Sriram2912@users.noreply.github.com>
2022-08-31 21:19:25 +0530

    Add files via upload

---
+++ arthemetic_operations.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	float a,b;
+	scanf("%f%f",&a,&b);
+	printf("Addition : %.2f",a+b);
+	printf("\nSubtraction : %.2f",a-b);
+	printf("\nMultiplication : %.2f",a*b);
+	printf("\nDivision : %.2f",a/b);
+	return 0;
+}
